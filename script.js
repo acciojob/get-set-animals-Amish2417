91ce1b4097c20f237dba427d7cc2f10725a0aecf
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-05-17 18:50:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 //complete this code
 class Animal {
-	constructor (species){
-		this._species = species;
-	}
-	get species() {
-       return this._species;
-    }
-	 makeSound(){
-		console.log(`${this._species} makes a sound`);
-	}
-	
+  constructor(species) {
+    this._species = species; 
+  }
+  get species() {
+    return this._species;
+  }
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
 class Dog extends Animal {
-	constructor (species){
-		this._species = species;
-	}
-	 bark(){
-		console.log("woof");
-	}
+  constructor(species) {
+    super(species);
+  }
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	constructor (species){
-		this._species = species;
-	}
-	 purr(){
-		console.log("purr");
-	}
-	
+  constructor(species) {
+    super(species);
+  }
+  purr() {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
